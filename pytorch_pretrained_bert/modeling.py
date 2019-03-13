9f18b37f9334eefcde7d431c81a1c6443eaaef21
Kevin Lalande <klalande@stanford.edu>
2019-03-13 07:42:30 -0500

    QA_v4 concatenate multiple sequence output layers
    On QA, rather than just taking the 768 hidden state of the final transformer block (Layer 12), concatenate multiple layers 9-12 to get an input of 4*768=3072...see if this helps performance

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -1314,3 +1314,102 @@ class BertForQuestionAnswering_v3(BertPreTrainedModel):
             return total_loss
         else:
             return start_logits, end_logits
+
+class BertForQuestionAnswering_v4(BertPreTrainedModel):
+    """BERT model for Question Answering (span extraction).
+    This module is composed of the BERT model with a linear layer on top of
+    the sequence output that computes start_logits and end_logits
+    KML: 3-Layer QA with input concatenated from multiple BERT attention block layers, rather than just the last layer
+
+    Params:
+        `config`: a BertConfig class instance with the configuration to build a new model.
+
+    Inputs:
+        `input_ids`: a torch.LongTensor of shape [batch_size, sequence_length]
+            with the word token indices in the vocabulary(see the tokens preprocessing logic in the scripts
+            `extract_features.py`, `run_classifier.py` and `run_squad.py`)
+        `token_type_ids`: an optional torch.LongTensor of shape [batch_size, sequence_length] with the token
+            types indices selected in [0, 1]. Type 0 corresponds to a `sentence A` and type 1 corresponds to
+            a `sentence B` token (see BERT paper for more details).
+        `attention_mask`: an optional torch.LongTensor of shape [batch_size, sequence_length] with indices
+            selected in [0, 1]. It's a mask to be used if the input sequence length is smaller than the max
+            input sequence length in the current batch. It's the mask that we typically use for attention when
+            a batch has varying length sentences.
+        `start_positions`: position of the first token for the labeled span: torch.LongTensor of shape [batch_size].
+            Positions are clamped to the length of the sequence and position outside of the sequence are not taken
+            into account for computing the loss.
+        `end_positions`: position of the last token for the labeled span: torch.LongTensor of shape [batch_size].
+            Positions are clamped to the length of the sequence and position outside of the sequence are not taken
+            into account for computing the loss.
+
+    Outputs:
+        if `start_positions` and `end_positions` are not `None`:
+            Outputs the total_loss which is the sum of the CrossEntropy loss for the start and end token positions.
+        if `start_positions` or `end_positions` is `None`:
+            Outputs a tuple of start_logits, end_logits which are the logits respectively for the start and end
+            position tokens of shape [batch_size, sequence_length].
+
+    Example usage:
+    ```python
+    # Already been converted into WordPiece token ids
+    input_ids = torch.LongTensor([[31, 51, 99], [15, 5, 0]])
+    input_mask = torch.LongTensor([[1, 1, 1], [1, 1, 0]])
+    token_type_ids = torch.LongTensor([[0, 0, 1], [0, 1, 0]])
+
+    config = BertConfig(vocab_size_or_config_json_file=32000, hidden_size=768,
+        num_hidden_layers=12, num_attention_heads=12, intermediate_size=3072)
+
+    model = BertForQuestionAnswering(config)
+    start_logits, end_logits = model(input_ids, token_type_ids, input_mask)
+    ```
+    """
+    def __init__(self, config):
+        super(BertForQuestionAnswering_v4, self).__init__(config)
+        self.bert = BertModel(config)
+        self.num_layers = 4  # KML concat last 4 BERT transformer layers
+        self.dropout0 = nn.Dropout(config.hidden_dropout_prob)
+        self.qa_intermediate1 = nn.Linear(self.num_layers * int(config.hidden_size / 1), self.num_layers * int(config.hidden_size / 2))
+        self.dropout1 = nn.Dropout(config.hidden_dropout_prob)
+        self.qa_intermediate2 = nn.Linear(self.num_layers * int(config.hidden_size / 2), self.num_layers * int(config.hidden_size / 4))
+        self.dropout2 = nn.Dropout(config.hidden_dropout_prob)
+        self.qa_outputs = nn.Linear(self.num_layers * int(config.hidden_size / 4), 2)
+        self.apply(self.init_bert_weights)
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, start_positions=None, end_positions=None):
+        sequence_output, _ = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=True)
+        # build concatenated hidden states for last n layers
+        for i in range(self.num_layers):
+            if i == 0:
+                concat_sequence_output = sequence_output[-1]
+            else:
+                concat_sequence_output = torch.cat((concat_sequence_output, sequence_output[-1-i]), 2)
+        
+        logits0 = self.dropout0(concat_sequence_output)        
+        logits1 = self.qa_intermediate1(logits0)
+        logits1 = self.dropout1(logits1)
+        logits2 = self.qa_intermediate2(logits1)
+        logits2 = self.dropout2(logits2)
+        logits = self.qa_outputs(logits2)
+
+        start_logits, end_logits = logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+
+        if start_positions is not None and end_positions is not None:
+            # If we are on multi-GPU, split add a dimension
+            if len(start_positions.size()) > 1:
+                start_positions = start_positions.squeeze(-1)
+            if len(end_positions.size()) > 1:
+                end_positions = end_positions.squeeze(-1)
+            # sometimes the start/end positions are outside our model inputs, we ignore these terms
+            ignored_index = start_logits.size(1)
+            start_positions.clamp_(0, ignored_index)
+            end_positions.clamp_(0, ignored_index)
+
+            loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
+            start_loss = loss_fct(start_logits, start_positions)
+            end_loss = loss_fct(end_logits, end_positions)
+            total_loss = (start_loss + end_loss) / 2
+            return total_loss
+        else:
+            return start_logits, end_logits
